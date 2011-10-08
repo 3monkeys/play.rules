28d2978bbf042cae9cd8e88008dbc6bbec553986
Philippe Charriere <ph.charriere@gmail.com>
2011-10-08 11:31:58 +0200

    ...

--- mobile.js
+++ mobile.js
@@ -33,6 +33,26 @@ function washCode(data){
     return data;
 }
 
+
+var loic = "", k33g = "", playframework = "";
+tw.search('#playframework',function(data){
+    for(var i = 0; i < 3; i+=1){
+		playframework += '<li><span> (' + data[i].user + ')</span> &raquo; ' + data[i].msg+'</li>';
+    }
+	tw.search('from:loic_d',function(data){
+        for(var i = 0; i <3; i+=1){
+			loic += '<li>' + data[i].msg+'</li>';
+        }
+		tw.search('from:k33g_org',function(data){
+	        for(var i = 0; i <3; i+=1){
+				k33g += '<li>' + data[i].msg+'</li>';
+	        }
+	    });
+    });
+
+});
+
+
 var Store = Class({
 	constructor : function Store (user,repo, callBk) {
 		var that = this;
@@ -71,37 +91,20 @@ var Store = Class({
 // required
 jo.load();
 
-// not required
-jo.setDebug(true);
 
 // placed in a module pattern, not a terrible idea for application level code
 var App = (function() {
 	var stack;
-	var scn;
-	var button;
-	var backbutton;
 	var page;
-	var login;
-	var test;
-	var more;
-	var option;
-	var select;
-	var moreback;
-	var urldata;
-	 window.list;
+	window.list;
 	var menu;
-	var cssnode;
-//	var blipsound = new joSound("blip2.wav");
-//	var bloopsound = new joSound("blip0.wav");
-	var cancelbutton;
-	var testds;
+	var twitts;
+	//var cssnode;
 
 	function init() {		
 
-		cssnode = joDOM.applyCSS(".htmlgroup { background: #fff; }");
-		
+		//cssnode = joDOM.applyCSS(".htmlgroup { background: #fff; }");
 
-		//var toolbar;
 		var nav;
 		
 		if (jo.matchPlatform("hpwos") && typeof PalmSystem === 'undefined')
@@ -113,7 +116,6 @@ var App = (function() {
 					nav = new joNavbar(),
 					stack = new joStackScroller()
 				])
-				//toolbar = new joToolbar("This is a footer, neat huh?")
 			]).setStyle({position: "absolute", top: "0", left: "0", bottom: "0", right: "0"})
 		);
 		
@@ -129,23 +131,12 @@ var App = (function() {
 		});
 		---*/
 
-		
-		// some arbitrary HTML shoved into a joHTML control
-		var html = new joHTML('<h1>Disclaimer</h1><p>This is a disclaimer. For more information, you can check <a href="moreinfo.html">this <b>file</b></a> for more info, or try your luck with <a href="someotherfile.html">this file</a>.');
-		var htmlgroup;
-		
-		var selectedDoc, selectedLabel;
-		
+		var html = new joHTML('');
+				
 		page = new joCard([
-			//selectedLabel = new joLabel("HTML Control : "),
 			html
-			//htmlgroup = new joGroup(html)
-		]).setTitle("Success");
+		]);
 		
-		
-		//htmlgroup.setStyle("htmlgroup");
-		
-		//backbutton.selectEvent.subscribe(back, this);
 		
 		/*--------Github----------*/
 		
@@ -154,30 +145,20 @@ var App = (function() {
 		window.list_items = [];
 
 		var gitHubStorage = new Store('3monkeys','play.rules', function(){
-			console.log("gitHubStorage");
-			
-			function getChapterContent(clickedLink) {			
-				gitHubStorage.getContent(clickedLink, function(content){
-					
-					//$("#details-container").html(converter.makeHtml(washCode(content)));
-					//$('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
-					
-					//showContent();
-					window.document.getElementById("scroller").scrollTop = 0;
-				});
-			}
 
 			gitHubStorage.getItemsOfBranch('master', function(items){ 
 				window.news = items.filter(function(item) { return item.name === "NEWS.md"; })[0];
 				gitHubStorage.getContent(news.name, function(content){
 					news.content = converter.makeHtml(washCode(content));
-					
-					//$("#details-container").html(news.content);
-					//$('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
-
-					twitter.removeClass("hide").addClass("show");
-
-					window.document.getElementById("scroller").scrollTop = 0
+					twitts.setData(
+						'<h2 class="border">@Twitter</h2>'+
+						'<h3 class="border">#PlayFramework</h3>'+
+						playframework+
+						'<h3 class="border">@loic_d</h2>'+
+						loic+
+						'<h3 class="border">@k33g_org</h3>'+
+						k33g
+					);
 				});			
 
 				var livre  = items.filter(function(item) { return item.name === "livre"; })[0];
@@ -186,14 +167,8 @@ var App = (function() {
 					list_items.push({title:"<b>Voir les news</b>", id:"NEWS"});
 					items.forEach(function(item) {
 						if(item.type ==='tree') {
-							//{ title: "Form Widgets", id: "login" },
 							
 							list_items.push({title:item.name, id:{sha:item.sha,name:item.name}});
-							
-							//console.log("###",{title:item.name, id:item.sha});
-							//$('#toc').append('<li class="part" style="width:100%;padding-left:0px;" id="li_'+item.sha+'"><b style="padding-left:5px">'+item.name+'</b></li>');
-
-							//$('#li_'+item.sha).append('<ul class="list" id="ul_'+item.sha+'"></li>');
 
 							gitHubStorage.getItemsOfItem(item, function(chapters){
 
@@ -204,23 +179,13 @@ var App = (function() {
 								chapters.forEach(function(chapter) {
 									
 									list_items[item.name].push({title:chapter.name, id:{sha:chapter.sha,name:pathPart+chapter.name}});
-									//console.log("chapter : ",{title:chapter.name, id:chapter.sha});
-
-									//$('#ul_'+item.sha).append('<li style="width:100%" id="li_'+chapter.sha+'"><a href="#'+pathPart+chapter.name+'">'+chapter.name+'</a></li>')
-									
-									//$('#li_'+chapter.sha)
-									//.bind('click',function(element){ getChapterContent(pathPart+chapter.name); });
 
 								});
-								//list.setData(list_items);
+
 							});
 						} else { //Blob
 							list_items.push({title:item.name, id:{sha:item.sha,name:path+item.name}});
-							//console.log(">>>",{title:item.name, id:item.sha});
-							/*$('#toc')
-							.append('<li id="li_'+item.sha+'"><a href="#'+path+item.name+'">'+item.name+'</a></li>');
-							$('#li_'+item.sha)
-							.bind('click',function(element){ getChapterContent(path+item.name); });*/
+
 						}
 						list.setData(list_items);
 					});
@@ -234,8 +199,9 @@ var App = (function() {
 		menu = new joCard([
 			list = new joMenu([
 				{ title: "Patientez pendant le chargement ...", id: "" }
-			])
-			//TDMButton = new joButton("Les News ...")
+			]),
+			new joHTML('<br>'),
+			twitts = new joHTML()
 		]).setTitle("Play.Rules<b>!</b>&raquo; eBook");
 		menu.activate = function() {
 			// maybe this should be built into joMenu...
@@ -243,10 +209,7 @@ var App = (function() {
 		};
 
 		list.selectEvent.subscribe(function(id) {
-			console.log(id);
 			if(list_items[id.name]){
-				//TDMButton.setData("<< Back");
-				console.log(list_items[id.name]);
 				list.setData(list_items[id.name]);
 				list.deselect();
 			} else {
@@ -256,11 +219,12 @@ var App = (function() {
 					
 				}else {
 					if(id==="NEWS") {
-						list.setData(list_items);
+						html.setData('<br>' + news.content);
+						$('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
+						
 						list.deselect();
+						stack.push(page);
 					} else {
-						//selectedLabel.setData(id.name);
-						page.setTitle(id.name);
 						
 						gitHubStorage.getContent(id.name, function(content){
 							//console.log(id.name);
@@ -269,30 +233,11 @@ var App = (function() {
 							stack.push(page);
 						});
 						
-
 					}
 				}
-			
 			}
-
-
 		}, this);
-		
-
-			
-		
-		/*TDMButton.selectEvent.subscribe(function() {
-			TDMButton.setData("Les News ...");
-			list.setData(list_items);
-		}, this);*/
-
-
-		//backbutton.selectEvent.subscribe(back, this);
-		html.selectEvent.subscribe(link, this);
-		
-		stack.pushEvent.subscribe(blip, this);
-		stack.popEvent.subscribe(bloop, this);
-		
+				
 		joGesture.forwardEvent.subscribe(stack.forward, stack);
 		joGesture.backEvent.subscribe(stack.pop, stack);
 		
@@ -304,35 +249,12 @@ var App = (function() {
 		stack.push(menu);
 	}
 	
-	function blip() {
-//		blipsound.play();
-	}
-	
-	function bloop() {
-//		bloopsound.play();
-	}
-	
-	function link(href) {
-		joLog("HTML link clicked: " + href);
-		urldata.setData(href);
-		stack.push(more);
-	}
-	
-	function click() {
-		stack.push(page);
-	}
-	
 	function back() {
 		stack.pop();
 	}
 	
 	// public stuff
 	return {
-		"init": init,
-		"getData": function() { return testds; },
-		"getStack": function() { return stack; },
-		"getButton": function() { return button; },
-		"getSelect": function() { return select; },
-		"getOption": function() { return option; }
+		"init": init
 	}
 }());
